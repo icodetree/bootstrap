33641794eff359aee736426336da7bbabf173fe9
icodetree <icodetree@naver.com>
2022-11-14 17:39:58 +0900

    add setting css

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,49 @@
-const path = require('path')
+const miniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
 
 module.exports = {
-  entry: './src/js/main.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  devServer: {
-    static: path.resolve(__dirname, 'dist'),
-    port: 8080,
-    hot: true
-  },
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          {
-            loader: 'style-loader'
-          },
-          {
-            loader: 'css-loader'
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              postcssOptions: {
-                plugins: () => [
-                  require('autoprefixer')
-                ]
-              }
-            }
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: true,
-              sassOptions: {
-                quietDeps: ["node_modules/bootstrap/**/*.scss"],
-              }
-            }
-          }
-        ]
-      }
-    ]
-  }
-}
+    mode: "production",
+    entry: "./src/js/main.js",
+    plugins: [new miniCssExtractPlugin()],
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    devServer: {
+        static: path.resolve(__dirname, "dist"),
+        port: 8080,
+        hot: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    {
+                        loader: miniCssExtractPlugin.loader,
+                    },
+                    {
+                        loader: "css-loader",
+                    },
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            postcssOptions: {
+                                plugins: () => [require("autoprefixer")],
+                            },
+                        },
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            sourceMap: true,
+                            sassOptions: {
+                                quietDeps: ["node_modules/bootstrap/**/*.scss"],
+                            },
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+};
