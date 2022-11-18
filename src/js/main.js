8a8a305b46ba73dca6a4d4a83a9579b6a20ed18a
hoyoung byon <bhy8407@gmail.com>
2022-11-18 23:40:31 +0900

    add slide update

--- src/js/main.js
+++ src/js/main.js
@@ -2,19 +2,30 @@
 import '../scss/default.scss';
 
 // Import only the Bootstrap components we need
-import { Util, Dropdown, Offcanvas, Popover } from 'bootstrap';
+import {Util, Dropdown, Offcanvas, Popover} from 'bootstrap';
 
 // Create an example popover
-document.querySelectorAll('[data-bs-toggle="popover"]').forEach(popover => {
-  new Popover(popover)
+document.querySelectorAll ('[data-bs-toggle="popover"]').forEach (popover => {
+  new Popover (popover);
 });
 
-
-var swiper = new Swiper(".swiperType_01", {
-  slidesPerView: 3,
-  spaceBetween: 30,
+// swiperType_01
+const swiper_1 = new Swiper ('.swiperType_01', {
+  loop: true,
+  slidesPerView: 1.8,
+  spaceBetween: 25,
+  centeredSlides: false,
   pagination: {
-    el: ".swiperType_01 .swiper-pagination",
+    el: '.swiperType_01 .swiper-pagination',
     clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1.1,
+    },
+
+    960: {
+      slidesPerView: 2.1,
+    },
   },
 });
