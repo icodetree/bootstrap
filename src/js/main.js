514e8f06620f3c5294d40f7d142b3228b283c81d
hoyoung byon <bhy8407@gmail.com>
2022-12-02 01:36:30 +0900

    add index modify 1st final

--- src/js/main.js
+++ src/js/main.js
@@ -2,15 +2,15 @@
 import '../scss/default.scss';
 
 // Import only the Bootstrap components we need
-import { Util, Dropdown, Offcanvas, Popover } from 'bootstrap';
+import {Util, Dropdown, Offcanvas, Popover} from 'bootstrap';
 
 // Create an example popover
-document.querySelectorAll('[data-bs-toggle="popover"]').forEach(popover => {
-  new Popover(popover);
+document.querySelectorAll ('[data-bs-toggle="popover"]').forEach (popover => {
+  new Popover (popover);
 });
 
 // myNoteSlide
-const myNoteSlide = new Swiper('.myNoteSlide .swiperType_01', {
+const myNoteSlide = new Swiper ('.myNoteSlide .swiperType_01', {
   slidesPerView: 1.8,
   spaceBetween: 25,
   centeredSlides: false,
@@ -28,13 +28,37 @@ const myNoteSlide = new Swiper('.myNoteSlide .swiperType_01', {
       slidesPerView: 1.1,
     },
     540: {
-      slidesPerView: 2.1,
+      slidesPerView: 2,
+    },
+  },
+});
+
+// myNoteAddSlide
+const myNoteAddSlide = new Swiper ('.myNoteAddSlide .swiperType_01', {
+  slidesPerView: 1.8,
+  spaceBetween: 25,
+  centeredSlides: false,
+  pagination: {
+    el: '.myNoteAddSlide .swiperType_01 .swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.myNoteAddSlide .swiper-button-next',
+    prevEl: '.myNoteAddSlide .swiper-button-prev',
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1.1,
+    },
+    540: {
+      slidesPerView: 2,
     },
   },
 });
 
 // contentTitleSlide
-const swiper_title = new Swiper('.contentTitleSlide .swiperType_02', {
+const swiper_title = new Swiper ('.contentTitleSlide .swiperType_02', {
   loop: true,
   slidesPerView: 1.8,
   spaceBetween: 20,
@@ -56,7 +80,7 @@ const swiper_title = new Swiper('.contentTitleSlide .swiperType_02', {
 });
 
 // cardSlide
-const swiper_card = new Swiper('.cardSlide .swiperType_card', {
+const swiper_card = new Swiper ('.cardSlide .swiperType_card', {
   loop: true,
   slidesPerView: 1.8,
   spaceBetween: 20,
@@ -64,6 +88,10 @@ const swiper_card = new Swiper('.cardSlide .swiperType_card', {
   pagination: {
     el: '.cardSlide .swiperType_card .swiper-pagination',
     clickable: true,
+  },
+  navigation: {
+    nextEl: '.cardSlide .swiper-button-next',
+    prevEl: '.cardSlide .swiper-button-prev',
   },
 
   breakpoints: {
@@ -83,7 +111,7 @@ const swiper_card = new Swiper('.cardSlide .swiperType_card', {
 });
 
 // thumbNailSlide
-const swiper_thumb = new Swiper('.thumbNailSlide .swiperType_thumb', {
+const swiper_thumb = new Swiper ('.thumbNailSlide .swiperType_thumb', {
   loop: true,
   spaceBetween: 25,
   centeredSlides: false,
@@ -94,7 +122,7 @@ const swiper_thumb = new Swiper('.thumbNailSlide .swiperType_thumb', {
 
   breakpoints: {
     0: {
-      slidesPerView: 1.4,
+      slidesPerView: 1.2,
     },
     540: {
       slidesPerView: 3,
@@ -106,8 +134,8 @@ const swiper_thumb = new Swiper('.thumbNailSlide .swiperType_thumb', {
 (() => {
   // Category TabList only DeskTop
   class TabList {
-    constructor(opt) {
-      this.tab = document.querySelector('.' + opt.elmName);
+    constructor (opt) {
+      this.tab = document.querySelector ('.' + opt.elmName);
       this.tabul = this.tab.firstElementChild;
       this.tabLi = [...this.tabul.children];
       this.target = null;
@@ -116,258 +144,260 @@ const swiper_thumb = new Swiper('.thumbNailSlide .swiperType_thumb', {
 
       this.active = 'active';
     }
-    init() {
-      this.tabLi.map(elm => {
+    init () {
+      this.tabLi.map (elm => {
         this.target = elm;
-        this.mouseEvent();
+        this.mouseEvent ();
       });
     }
-    mouseEvent() {
-      this.target.addEventListener('click', e => this.eventHandler(e));
+    mouseEvent () {
+      this.target.addEventListener ('click', e => this.eventHandler (e));
     }
-    eventHandler(e) {
-      e.preventDefault();
+    eventHandler (e) {
+      e.preventDefault ();
       let type = e.type;
 
-      if (e.target !== e.currentTarget.querySelector('.subtit')) return;
+      if (e.target !== e.currentTarget.querySelector ('.subtit')) return;
       this.parent = e.currentTarget.firstElementChild;
 
-      console.log(e.target, e.currentTarget.querySelector('.subtit'));
+      console.log (e.target, e.currentTarget.querySelector ('.subtit'));
 
-      if (!this.parent.classList.contains(this.active)) {
-        this.open();
+      if (!this.parent.classList.contains (this.active)) {
+        this.open ();
       } else {
-        this.close();
+        this.close ();
       }
     }
-    open() {
-      this.tabLi.map((elm, idx) => {
+    open () {
+      this.tabLi.map ((elm, idx) => {
         this.elm = elm.firstElementChild;
-        this.elm.classList.remove(this.active);
+        this.elm.classList.remove (this.active);
       });
-      this.parent.classList.add(this.active);
-      this.tab.classList.add(this.active);
+      this.parent.classList.add (this.active);
+      this.tab.classList.add (this.active);
     }
-    close() {
-      this.tabLi.map((elm, idx) => {
+    close () {
+      this.tabLi.map ((elm, idx) => {
         this.elm = elm.firstElementChild;
-        this.elm.classList.remove(this.active);
+        this.elm.classList.remove (this.active);
       });
 
-      this.parent.classList.remove(this.active);
-      this.tab.classList.remove(this.active);
+      this.parent.classList.remove (this.active);
+      this.tab.classList.remove (this.active);
     }
   }
 
   // AddSearch only DeskTop
   // Common Top button
   class AddSearch {
-    constructor(opt) {
-      this.html = document.querySelector('html');
-      this.body = document.querySelector('body');
-      this.addBtn = document.querySelector('.' + opt.elmName);
-      this.addSearch = document.querySelector('.addSearch');
+    constructor (opt) {
+      this.html = document.querySelector ('html');
+      this.body = document.querySelector ('body');
+      this.addBtn = document.querySelector ('.' + opt.elmName);
+      this.addSearch = document.querySelector ('.addSearch');
 
       this.active = 'active';
     }
-    init() {
-      this.mouseEvent();
+    init () {
+      this.mouseEvent ();
     }
-    mouseEvent() {
-      this.body.addEventListener('click', e => this.eventHandler(e));
-      window.addEventListener('scroll', e => this.scrollEvent(e));
+    mouseEvent () {
+      this.body.addEventListener ('click', e => this.eventHandler (e));
+      window.addEventListener ('scroll', e => this.scrollEvent (e));
     }
-    eventHandler(e) {
-      e.preventDefault();
+    eventHandler (e) {
+      e.preventDefault ();
 
       this.parent = e.target.parentElement;
 
       if (
-        !this.parent.classList.contains(this.active) &&
-        e.target === e.currentTarget.querySelector('.addSearchBtn')
+        !this.parent.classList.contains (this.active) &&
+        e.target === e.currentTarget.querySelector ('.addSearchBtn')
       ) {
-        this.open();
+        this.open ();
       } else {
         if (
-          e.target === e.currentTarget.querySelector('.input-search') ||
-          e.target === e.currentTarget.querySelector('.btn-search')
+          e.target === e.currentTarget.querySelector ('.input-search') ||
+          e.target === e.currentTarget.querySelector ('.btn-search')
         )
           return;
-        this.close();
+        this.close ();
       }
     }
-    open() {
-      this.addBtn.classList.add(this.active);
-      this.addSearch.classList.add(this.active);
+    open () {
+      this.addBtn.classList.add (this.active);
+      this.addSearch.classList.add (this.active);
     }
-    close() {
-      this.addBtn.classList.remove(this.active);
-      this.addSearch.classList.remove(this.active);
+    close () {
+      this.addBtn.classList.remove (this.active);
+      this.addSearch.classList.remove (this.active);
     }
-    scrollEvent(e) {
-      e.preventDefault();
+    scrollEvent (e) {
+      e.preventDefault ();
 
-      this.topBtn = document.querySelector('.btnTop');
-      this.headerH = document.querySelector('.header').clientHeight;
-      this.visualH = document.querySelector('.visual').clientHeight;
+      this.topBtn = document.querySelector ('.btnTop');
+      this.headerH = document.querySelector ('.header').clientHeight;
+      this.visualH = document.querySelector ('.visual').clientHeight;
 
       if (this.html.scrollTop > this.visualH + this.visualH) {
-        this.topBtn.classList.add(this.active);
-        this.topBtn.addEventListener('click', e => this.moveTop(e));
+        this.topBtn.classList.add (this.active);
+        this.topBtn.addEventListener ('click', e => this.moveTop (e));
       } else {
-        this.topBtn.classList.remove(this.active);
+        this.topBtn.classList.remove (this.active);
       }
     }
-    moveTop() {
-      window.scrollTo({ top: 0, behavior: 'smooth' });
+    moveTop () {
+      window.scrollTo ({top: 0, behavior: 'smooth'});
     }
   }
 
   // Common ChangeView
   class ChangeView {
-    constructor(opt) {
-      this.targetBtn = document.querySelector('.' + opt.elmName);
-      this.swiper = document.querySelector('.myNoteSlide');
+    constructor (opt) {
+      this.targetBtn = document.querySelector ('.' + opt.elmName);
+      this.swiper = document.querySelector ('.myNoteSlide');
       this.active = 'active';
       this.target = null;
       this.parent = null;
     }
-    init() {
-      this.mouseEvent();
+    init () {
+      this.mouseEvent ();
     }
-    mouseEvent() {
-      this.targetBtn.addEventListener('click', e => this.eventHandler(e));
+    mouseEvent () {
+      this.targetBtn.addEventListener ('click', e => this.eventHandler (e));
     }
-    eventHandler(e) {
-      e.preventDefault();
+    eventHandler (e) {
+      e.preventDefault ();
       this.target = e.currentTarget.firstElementChild;
       this.parent = e.currentTarget;
 
-      if (!this.parent.classList.contains(this.active)) {
-        this.open();
+      if (!this.parent.classList.contains (this.active)) {
+        this.open ();
       } else {
-        this.close();
+        this.close ();
       }
     }
-    open() {
-      this.targetBtn.classList.add(this.active);
+    open () {
+      this.targetBtn.classList.add (this.active);
       this.target.textContent = `전체보기 취소`;
-      this.swiper.classList.add(this.active);
-      myNoteSlide.disable();
+      this.swiper.classList.add (this.active);
+      myNoteSlide.disable ();
     }
-    close() {
-      this.targetBtn.classList.remove(this.active);
+    close () {
+      this.targetBtn.classList.remove (this.active);
       this.target.textContent = `전체보기`;
-      this.swiper.classList.remove(this.active);
-      myNoteSlide.enable();
+      this.swiper.classList.remove (this.active);
+      myNoteSlide.enable ();
     }
   }
 
   class Vocabulary {
-    constructor(opt) {
-      this.vocabulary = document.querySelector('.' + opt.elmName);
-      this.vocList = [...document.querySelectorAll('.vocabulary')];
+    constructor (opt) {
+      this.vocabulary = document.querySelector ('.' + opt.elmName);
+      this.vocList = [...document.querySelectorAll ('.vocabulary')];
 
       this.target = null;
       this.active = 'active';
       this.parent = null;
       this.mother = null;
     }
-    init() {
-      this.vocList.map(elm => {
-        this.target = elm.querySelector('.addBtn');
-        this.mouseEvent();
+    init () {
+      this.vocList.map (elm => {
+        this.target = elm.querySelector ('.addBtn');
+        this.mouseEvent ();
       });
     }
-    mouseEvent() {
-      this.target.addEventListener('click', e => this.eventHandler(e));
+    mouseEvent () {
+      this.target.addEventListener ('click', e => this.eventHandler (e));
     }
-    eventHandler(e) {
-      e.preventDefault();
+    eventHandler (e) {
+      e.preventDefault ();
 
       this.parent = e.target;
-      this.mother = this.parent.closest('.vocabulary');
+      this.mother = this.parent.closest ('.vocabulary');
 
-      if (!this.parent.classList.contains(this.active)) {
-        this.open();
+      if (!this.parent.classList.contains (this.active)) {
+        this.open ();
       } else {
-        this.close();
+        this.close ();
       }
     }
-    open() {
-      this.parent.classList.add(this.active);
+    open () {
+      this.parent.classList.add (this.active);
       this.parent.textContent = `내 단어장에서 삭제하기`;
-      this.mother.classList.add(this.active);
+      this.mother.classList.add (this.active);
     }
-    close() {
-      this.parent.classList.remove(this.active);
+    close () {
+      this.parent.classList.remove (this.active);
       this.parent.textContent = `내 단어장에 추가하기`;
-      this.mother.classList.remove(this.active);
+      this.mother.classList.remove (this.active);
     }
   }
 
   // Toggle Button
   class Toggle {
-    constructor(opt) {
-      this.btn = [...document.querySelectorAll('#' + opt.elmName)];
-      this.closeBtn = document.querySelector(".popClose");
-      this.body = document.querySelector("body");
+    constructor (opt) {
+      this.btn = [...document.querySelectorAll ('#' + opt.elmName)];
+      this.closeBtn = [...document.querySelectorAll ('.popClose')];
+      this.body = document.querySelector ('body');
 
       this.target = null;
       this.overlay = null;
       this.parent = null;
       this.active = 'active';
       this.hidden = 'hidden';
+      this.elmBool = null;
     }
-    init() {
-      this.btn.forEach(elm => {
+    init () {
+      this.btn.forEach (elm => {
         this.target = elm;
-        this.mouseEvent();
+        this.mouseEvent ();
+      });
+      this.closeBtn.forEach (elm => {
+        elm.addEventListener ('click', e => this.close ());
       });
     }
-    mouseEvent() {
-      this.target.addEventListener('click', e => this.eventHandler(e));
-      this.closeBtn.addEventListener('click', e => this.close());
+    mouseEvent () {
+      this.target.addEventListener ('click', e => this.eventHandler (e));
     }
-    eventHandler(e) {
-      e.preventDefault();
+    eventHandler (e) {
+      e.preventDefault ();
       this.parent = e.currentTarget;
       this.overlay = this.parent.nextElementSibling;
+      this.elmBool = this.parent.closest ('.navbar') ? true : false;
 
-      if (!this.parent.classList.contains(this.active)) {
-        this.open();
+      if (!this.parent.classList.contains (this.active)) {
+        this.open ();
       } else {
-        this.close();
+        this.close ();
       }
     }
-    open() {
-      this.parent.classList.add(this.active);
-      this.overlay.classList.add(this.active);
-      this.body.classList.add(this.hidden);
+    open () {
+      this.parent.classList.add (this.active);
+      this.overlay.classList.add (this.active);
+      if (this.elmBool) this.body.classList.add (this.hidden);
     }
-    close() {
-      this.parent.classList.remove(this.active);
-      this.overlay.classList.remove(this.active);
-      this.body.classList.remove(this.hidden);
+    close () {
+      this.parent.classList.remove (this.active);
+      this.overlay.classList.remove (this.active);
+      if (this.elmBool) this.body.classList.remove (this.hidden);
     }
   }
 
+  window.addEventListener ('load', () => {
+    const tabList = new TabList ({elmName: 'TabList'});
+    tabList.init ();
 
+    const addSearch = new AddSearch ({elmName: 'addSearchBtn'});
+    addSearch.init ();
 
-  window.addEventListener('load', () => {
-    const tabList = new TabList({ elmName: 'TabList' });
-    tabList.init();
+    const changeView = new ChangeView ({elmName: 'changeViewBtn'});
+    changeView.init ();
 
-    const addSearch = new AddSearch({ elmName: 'addSearchBtn' });
-    addSearch.init();
+    const vocabulary = new Vocabulary ({elmName: 'vocabulary'});
+    vocabulary.init ();
 
-    const changeView = new ChangeView({ elmName: 'changeViewBtn' });
-    changeView.init();
-
-    const vocabulary = new Vocabulary({ elmName: 'vocabulary' });
-    vocabulary.init();
-
-    const toggle = new Toggle({ elmName: 'toggle' });
-    toggle.init();
+    const toggle = new Toggle ({elmName: 'toggle'});
+    toggle.init ();
   });
-})();
+}) ();
